a96239de3e7859ba660120190c6ba54f94b51de7
Leonardo Aucar <leonardoaucar@gmail.com>
2018-10-03 18:57:53 -0300

    CRUD for new restaurants and menu items

--- finalproject.py
+++ finalproject.py
@@ -1,44 +1,72 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect, flash
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 # Rotas para a aplicacao
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-    return 'this page will show all my restaurants'
+    restaurants = session.query(Restaurant).all()
+    return render_template('restaurants.html', restaurants=restaurants)
 
-@app.route('/restaurant/new/')
+@app.route('/restaurant/new/', methods=['GET','POST'])
 def newRestaurant():
-    return 'this page will be for making a new restaurant'
+    if request.method == 'POST':
+        newRestaurant = Restaurant(name = request.form['name'])
+        session.add(newRestaurant)
+        session.commit()
+        flash("new Restaurant created")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newRestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit/')
-def editRestaurant():
-    return 'this page will be for editing restaurants'
+def editRestaurant(restaurant_id):
+    return render_template('editRestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/delete/')
-def deleteRestaurant():
-    return 'this page will be for deleting restaurants'
+def deleteRestaurant(restaurant_id):
+    return render_template('deleteRestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
-    return 'this page will be for the menu'
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return 'this page is to create new menu items'
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        flash("new menu item created")
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id = restaurant_id)
 
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
-    return 'this page is to edit menu items'
+    return render_template('editMenuItem.html')
 
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
-    return 'this page is to delete menu items'
+    return render_template('deleteMenuItem.html', item=item)
 
 
 #====================!!!!=======================
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
